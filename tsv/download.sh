0a62e79d45f98a55d28f7432f421c8f854e0c8e7
Marcos Neves <neves64@icloud.com>
2024-10-18 01:29:00 -0300

    replace wget with aria2c

--- tsv/download.sh
+++ tsv/download.sh
@@ -1,10 +1,12 @@
 # Download IMDB CSV files only if changed
-wget -cN https://datasets.imdbws.com/title.principals.tsv.gz
-wget -cN https://datasets.imdbws.com/title.akas.tsv.gz
-wget -cN https://datasets.imdbws.com/name.basics.tsv.gz
-wget -cN https://datasets.imdbws.com/title.basics.tsv.gz
-wget -cN https://datasets.imdbws.com/title.crew.tsv.gz
-wget -cN https://datasets.imdbws.com/title.episode.tsv.gz
-wget -cN https://datasets.imdbws.com/title.ratings.tsv.gz
-
+# wget -cN https://datasets.imdbws.com/title.principals.tsv.gz
+# wget -cN https://datasets.imdbws.com/title.akas.tsv.gz
+# wget -cN https://datasets.imdbws.com/name.basics.tsv.gz
+# wget -cN https://datasets.imdbws.com/title.basics.tsv.gz
+# wget -cN https://datasets.imdbws.com/title.crew.tsv.gz
+# wget -cN https://datasets.imdbws.com/title.episode.tsv.gz
+# wget -cN https://datasets.imdbws.com/title.ratings.tsv.gz
+aria2c -c --optimize-concurrent-downloads -x10 -i urls.txt
 gunzip -kv *.gz
+mkdir -p tsv
+mv *.tsv tsv
